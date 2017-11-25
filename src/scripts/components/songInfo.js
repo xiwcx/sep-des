e7468e367e32838099c362b0a2ff36fbcba2bea1
Welch Canavan <welch.canavan@gmail.com>
2017-11-25 17:08:09 -0500

    update image/text toggle button

--- src/scripts/components/songInfo.js
+++ src/scripts/components/songInfo.js
@@ -16,6 +16,42 @@ class SongInfo extends React.Component {
     });
   }
 
+  renderPlay() {
+    return(
+      <button className='nav__button nav__button--audio' onClick={this.toggleAudio}>
+        <span className="visuallyhidden">
+          Play
+        </span>
+
+        <svg className='nav__button-icon'xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 72"><path d="M5,12.07,28.93,36,5,59.93V12.07M0,0V72L36,36,0,0Z"/></svg>
+      </button>
+    );
+  }
+
+  renderShowImage() {
+    return(
+      <button className="song-info__toggle-button" onClick={this.toggleView}>
+        <span className="visuallyhidden">
+          Show Image
+        </span>
+
+        <svg className="song-info__button-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36"><path d="M20.21,0H0V36H36V0ZM5,31V24.54l6-6L23.46,31Zm26,0h-.47l-16-16L11,11.47,7.46,15,5,17.47V5h9A16,16,0,0,0,30,21c.33,0,.67,0,1,0ZM30,16A11,11,0,0,1,19,5H31V16C30.67,16,30.34,16,30,16Z"/></svg>
+      </button>
+    );
+  }
+
+  renderShowText() {
+    return(
+      <button className="song-info__toggle-button" onClick={this.toggleView}>
+        <span className="visuallyhidden">
+          Show Text
+        </span>
+
+        <svg className="song-info__button-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 32"><g><rect width="36" height="5"/><rect y="27" width="31" height="5"/><rect y="9" width="31" height="5"/><rect y="18" width="36" height="5"/></g></svg>
+      </button>
+    );
+  }
+
   updateSong() {
     this.props.setSong(this.props.songId);
   }
@@ -34,6 +70,7 @@ class SongInfo extends React.Component {
             <img className="song-info__img" src={this.props.songImg} />
           </div>
         </div>
+
         <div className="song-info__panel song-info__panel--lyrics">
           <div className="song-info__content">
             <h1 className="song-info__title">{this.props.songTitle}</h1>
@@ -49,9 +86,10 @@ class SongInfo extends React.Component {
             </p>
           </div>
         </div>
-        <button className="song-info__toggle-button" onClick={this.toggleView}>
-          <span>Toggle</span>
-        </button>
+
+        {this.state.displayLyrics ?
+          this.renderShowImage()
+          : this.renderShowText()}
       </div>
     );
   }
