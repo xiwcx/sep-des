5451997d87dd133667b1039dda7acd65b7c6764c
Welch Canavan <welch.canavan@gmail.com>
2017-11-25 16:09:41 -0500

    add button icons

--- src/scripts/components/app.js
+++ src/scripts/components/app.js
@@ -67,6 +67,30 @@ class App extends React.Component {
     }
   }
 
+  renderPause() {
+    return(
+      <button className='nav__button nav__button--audio' onClick={this.toggleAudio}>
+        <span className="visuallyhidden">
+          Pause
+        </span>
+
+        <svg className='nav__button-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 72"><g><rect x="13" width="5" height="72"/><rect width="5" height="72"/></g></svg>
+      </button>
+    );
+  }
+
+  renderPlay() {
+    return(
+      <button className='nav__button nav__button--audio' onClick={this.toggleAudio}>
+        <span className="visuallyhidden">
+          Play
+        </span>
+
+        <svg className='nav__button-icon'xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 72"><path d="M5,12.07,28.93,36,5,59.93V12.07M0,0V72L36,36,0,0Z"/></svg>
+      </button>
+    );
+  }
+
   setSong(song) {
     this.setState({
       currentSong: song,
@@ -99,7 +123,7 @@ class App extends React.Component {
           >
             <div className="text__container">
               <div className="text__content">
-                <h1 className="h1">We Are Separate / We Are Desperate</h1>
+                <h1 className="h1">We Are Separate /<br /> We Are Desperate</h1>
                 <h2 className="h2 color-text-inverse">An Extended Play by Será</h2>
                 <ol className="ol__container">
                   <li className="ol__item">Stare And Slow</li>
@@ -175,15 +199,41 @@ class App extends React.Component {
         </main>
 
         <nav className="nav__container">
-          <div>
-            <button onClick={this.prevPage}>Previous Page</button>
-            <button onClick={this.nextPage}>Next Page</button>
-          </div>
-          <div>
-            <button onClick={this.prevSong}>Previous Song</button>
-            <button onClick={this.toggleAudio}>Toggle Audio</button>
-            <button onClick={this.nextSong}>Next Song</button>
-          </div>
+          <button className='nav__button nav__button--page nav__button--page-prev' onClick={this.prevPage}>
+            <span className="visuallyhidden">
+              Previous Page
+            </span>
+
+            <svg className="nav__button-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 72"><polygon points="36 64.93 7.07 36 36 7.07 36 0 0 36 36 72 36 64.93"/></svg>
+          </button>
+
+          <button className='nav__button nav__button--audio' onClick={this.prevSong}>
+            <span className="visuallyhidden">
+              Previous Song
+            </span>
+
+            <svg className="nav__button-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 41 72"><g><path d="M5,36,41,72V0ZM36,59.93,12.07,36,36,12.07Z"/><polygon points="0 0 0 72 5 72 5 36 5 0 0 0"/></g></svg>
+          </button>
+
+          {this.state.playing ?
+            this.renderPause()
+            : this.renderPlay()}
+
+          <button className='nav__button nav__button--audio' onClick={this.nextSong}>
+            <span className="visuallyhidden">
+              Next Song
+            </span>
+
+            <svg className="nav__button-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 41 72"><path d="M0,72,36,36,0,0ZM5,12.07,28.93,36,5,59.93Z"/><polygon points="36 0 36 36 36 72 41 72 41 0 36 0"/></svg>
+          </button>
+
+          <button className='nav__button nav__button--page nav__button--page-next' onClick={this.nextPage}>
+            <span className="visuallyhidden">
+              Next Page
+            </span>
+
+            <svg className="nav__button-icon nav__button--page" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 72"><polygon points="0 64.93 28.93 36 0 7.07 0 0 36 36 0 72 0 64.93"/></svg>
+          </button>
         </nav>
 
         <ReactHowler
